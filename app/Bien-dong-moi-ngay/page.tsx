7a9286306eee1965d1d0bee9cbf54af475f40411
havanphi <havanphi2020@gmail.com>
2026-05-05 16:50:58 +0700

    fix bien dong folder lowercase

--- app/Bien-dong-moi-ngay/page.tsx
+++ app/Bien-dong-moi-ngay/page.tsx
@@ -1,5 +1,8 @@
-import CategoryPage from "../components/CategoryPage";
-
 export default function Page() {
-  return <CategoryPage title="Biến động mỗi ngày" />;
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Biển động mỗi ngày</h1>
+      <p>Đây là trang chuyên mục biển động mỗi ngày</p>
+    </div>
+  );
 }
