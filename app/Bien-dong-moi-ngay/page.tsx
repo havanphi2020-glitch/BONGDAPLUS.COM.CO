0905a30b317343dc248599b08ddda09273be9d0c
havanphi <havanphi2020@gmail.com>
2026-05-05 16:46:40 +0700

    fix bien dong moi ngay page

--- app/Bien-dong-moi-ngay/page.tsx
+++ app/Bien-dong-moi-ngay/page.tsx
@@ -1,8 +1,5 @@
+import CategoryPage from "../components/CategoryPage";
+
 export default function Page() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Biển động mỗi ngày</h1>
-      <p>Đây là trang chuyên mục biển động mỗi ngày</p>
-    </div>
-  );
+  return <CategoryPage title="Biến động mỗi ngày" />;
 }
