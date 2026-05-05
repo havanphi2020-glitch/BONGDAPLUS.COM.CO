9ee5b7ef6d43ba88a7a899946ade0652aaf63ce6
havanphi <havanphi2020@gmail.com>
2026-05-05 16:25:02 +0700

    add bong da viet nam page

---
+++ app/bong-da-viet-nam/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Bóng đá Việt Nam</h1>
+      <p>Đây là trang chuyên mục bóng đá Việt Nam</p>
+    </div>
+  );
+}
